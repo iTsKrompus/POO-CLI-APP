ba7e69a3edec881b7797a3eb5074839262feed13
bt0080 <hugo.sanchez@alumnos.upm.es>
2023-11-23 11:07:11 +0100

    Creada View

--- src/main/java/upm/Console/View.java
+++ src/main/java/upm/Console/View.java
@@ -1,4 +1,23 @@
 package upm.Console;
 
 public class View {
+
+    public static final String PURPLE = "\u001B[35m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+    public static final String WHITE = "\u001B[37m";
+
+
+    public static final String BACKGROUND_RED = "\u001B[41m";
+
+    public void show(String message){
+        System.out.println(View.BLUE +" " + message + " " + View.RESET);
+    }
+    public void showBold(String message){
+        System.out.println((View.PURPLE + " " + message + " " + View.RESET));
+    }
+    public void ShowError(String message){
+        System.out.println(BACKGROUND_RED + View.WHITE + " " + message + " " + View.RESET);
+    }
+
 }
